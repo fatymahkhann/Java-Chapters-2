b7eae146c9361e179e1912b86569ca31030417e9
fatymahkhann <fatymahasan409@gmail.com>
2025-12-27 12:54:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+//Declear a variable called username.//
+var username;
+
+//Declear a variable called myname & assign to it a string that represents your fullname.//
+var myname = "Fatyma Hasan";
+
+//write script to//
+//  a) Declear a Js variable titled message.//
+//  b) assign hello world to variable message.//
+//  c) Display the message in alert box.//
+var message = "Hello World";
+alert(message);
+
+// Write a script to save students bio data in JS variables and show data in alert box.//
+var name = "Fatyma";
+alert(name);
+var age = 18;
+alert(age);
+var course = "Web Development";
+alert(course);
+
+//Write a script to display the following alert using one JS variable.//
+var Pizza = "Pizza \nPizz \nPiz \nPi \nP";
+alert(Pizza);
+
+//Declear a variable called email & assign to it a string that represent your email address.//
+var email = "fatymakhan@gmail.com";
+alert(email);
+
+//Declear a variable called book & give it the value A smarter way to learn js display the following message in alert box.//
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn js from the book " + book);
+
+//Write a script to display this in browser through js.//
+document.write("Yah! I can write HTML content through js");
+
+//Store following string in a variable and show in alert and browser through js.//
+var design = "------~~~//~~~-------";
+alert(design);
+document.write("<br>" + design);
+
